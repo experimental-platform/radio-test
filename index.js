a101de76736f8c0aeaf44eca33a8c40d923a197d
Hinnerk Haardt <haardt@information-control.de>
2015-10-06 19:04:34 +0200

    Initial working demo

---
+++ index.js
@@ -0,0 +1,50 @@
+var express = require('express'),
+  app = express(),
+  path = require('path'),
+  http = require('http').Server(app),
+  io = require('socket.io')(http),
+  serial = require('./serial.js');
+
+app.use(express.static(path.join(__dirname, './static')));
+
+
+io.on('connection', function (socket) {
+  console.log('User connected. Socket id %s', socket.id);
+
+  socket.on('join', function (rooms) {
+    console.log('Socket %s subscribed to %s', socket.id, rooms);
+    if (Array.isArray(rooms)) {
+      rooms.forEach(function (room) {
+        socket.join(room);
+      });
+    } else {
+      socket.join(rooms);
+    }
+  });
+
+  socket.on('leave', function (rooms) {
+    console.log('Socket %s unsubscribed from %s', socket.id, rooms);
+    if (Array.isArray(rooms)) {
+      rooms.forEach(function (room) {
+        socket.leave(room);
+      });
+    } else {
+      socket.leave(rooms);
+    }
+  });
+
+  socket.on('disconnect', function () {
+    console.log('User disconnected. %s. Socket id %s', socket.id);
+  });
+});
+
+
+serial.start(function (message) {
+  console.log(message);
+  io.emit('data received', message);
+});
+
+http.listen(process.env.PORT || 5000, function () {
+  console.log('listening on: ' + process.env.PORT || 5000);
+});
+
--- serial.js
+++ serial.js
@@ -2,7 +2,7 @@ var async = require("async");
 var serialport = require("serialport");
 var SerialPort = serialport.SerialPort;
 
-var port = new SerialPort("/dev/ttyUSB0", {
+var port = new SerialPort("/dev/tty.usbserial-DA01ID01", {
   baudrate: 115200,
   databits: 8,
   stopbits: 1,
@@ -11,41 +11,47 @@ var port = new SerialPort("/dev/ttyUSB0", {
   //parser: serialport.parsers.raw,
   platformOptions: {
     vmin: 0
-}
+  }
 });
-
-var lineBuffer = [];
-var lastLine = null;
 
 function parsePacket(packet) {
   console.log(packet.length + ' pieces of data in the package')
 }
 
-function parseLine(line) {
-  if (line === 'Program started\r') {
-    return;
-  } else if(line === 'End of recordedBits\r') {
-    parsePacket(lineBuffer)
-    lineBuffer = []
-  } else {
-    var arr = line.split(' ')
-    var datum = {state: arr[1], time: arr[2] }
-    lineBuffer.push(datum)
+function makeParser(cb) {
+  var lineBuffer = [];
+  return function parseLine(line) {
+    // TODO: track line numbers
+    if (line === 'Program started\r') {
+      console.log("Program started");
+      return false;
+    } else if (line === 'End of recordedBits\r') {
+      cb(lineBuffer);
+      // console.log("Bits logged");
+      lineBuffer = [];
+    } else {
+      var arr = line.split(' ');
+      var datum = {state: arr[1], time: arr[2]};
+      lineBuffer.push(datum);
+      // console.log("Bit received");
+    }
   }
 }
 
-port.on("open", function (error) {
-  if ( error ) {
-    console.log('failed to open: '+error);
-  } else {
-    console.log('Port opened');
 
-    //port.on('data', function(line) { console.log(line); });
-    port.on('data', parseLine);
-  }
-});
+exports.start = function (onChange) {
+  port.on("open", function (error) {
+    if (error) {
+      console.log('failed to open: ' + error);
+    } else {
+      console.log('Port opened');
 
-port.on('error', function(error) {
-  console.log('error: ' + error);
-});
+      //port.on('data', function(line) { console.log(line); });
+      port.on('data', makeParser(onChange));
+    }
+  });
 
+  port.on('error', function (error) {
+    console.log('error: ' + error);
+  });
+};
