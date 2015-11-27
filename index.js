c137a885e6093b35c077d1bde3de6020479d96e7
Christopher Blum <christopher.blum@spellboy.com>
2015-11-27 12:21:56 +0100

    Greatly simplify radio test app (for readability)

--- index.js
+++ index.js
@@ -1,80 +1,66 @@
 var express = require('express'),
-  app = express(),
-  path = require('path'),
-  http = require('http').Server(app),
-  io = require('socket.io')(http),
-  request = require('request').defaults({json: true}),
-  _ = require('underscore'),
-  serial = require('./serial.js'),
-  storage = require('./storage'),
-  analyse = require('./analyse').analyse;
+    app = express(),
+    path = require('path'),
+    http = require('http').Server(app),
+    io = require('socket.io')(http),
+    _ = require('underscore'),
+    serial = require('./serial.js'),
+    storage = require('./storage'),
+    analyse = require('./analyse').analyse;
 
 
-data = storage.read(function (data) {
-  // express
-  // static file server
+storage.read(function(data) {
+  function save() {
+    // remove all entries without a name
+    data = _.omit(data, function (entry) {
+      return entry.name === null;
+    });
+    storage.write(data);
+  }
+
   app.use(express.static(path.join(__dirname, './static')));
   // socket.io handlers
   io.on('connection', function (socket) {
     console.log('User connected. Socket id %s', socket.id);
     // send all known
     io.emit('known signals', data);
-    socket.on('send signal', function (signal_id) {
+
+    socket.on('send signal', function(signal_id) {
       var signal = data[signal_id];
-      if (typeof signal.webhook_send == "string") {
-        console.log("Calling webhook on send: ", signal['webhook_send']);
-        request
-          .get(signal['webhook_send'])
-          .on('error', function (err) {
-            console.log(err)
-          })
-          .on('response', function (response) {
-            // TODO: Do something with the response?
-            console.log("Webhook status code: ", response.statusCode);
-          });
-      }
       console.log("Sending Signal to ", signal.name, " (", signal_id, ")");
       serial.send(signal.timings);
     });
-    socket.on('renamed signal', function (signal) {
+
+    socket.on('renamed signal', function(signal) {
       console.log("NEW NAME: '", signal.name, "' for '", signal.identity, "'");
       data[signal.identity] = signal;
-      // remove all entries without a name
-      data = _.omit(data, function (entry) {
-        return entry.name === null;
-      });
-      storage.write(data);
+      save();
     });
+
+    socket.on('delete signal', function(signal) {
+      delete data[signal.identity];
+      save();
+    });
+
     socket.on('disconnect', function () {
       console.log('User disconnected. %s. Socket id %s', socket.id);
     });
   });
-// serial console handlers
+
   serial.start(function (signal) {
-    if (signal) {
-      signal = analyse(signal);
-      if (data[signal.identity]) {
-        io.emit('known signal', data[signal.identity]);
-        // TODO: compare signal quality and replace bad recordings
-        var webhook = data[signal.identity]["webhook_receive"];
-        if (typeof webhook == "string") {
-          console.log("Calling webhook on receive: ", webhook);
-          request
-            .post(webhook, signal)
-            .on('error', function (err) {
-              console.log("Webhook ERROR: ", err)
-            })
-            .on('response', function (response) {
-              // TODO: Do something with the response?
-              console.log("Webhook status code: ", response.statusCode);
-            });
-        }
-      } else {
-        io.emit('new signal', signal);
-        io.emit('known signal', signal);
-      }
+    if (!signal) {
+      return;
+    }
+    signal = analyse(signal);
+    if (data[signal.identity]) {
+      io.emit('known signal', data[signal.identity]);
+      // TODO: compare signal quality and replace bad recordings
+    } else {
+      io.emit('new signal', signal);
+      io.emit('known signal', signal);
     }
   });
+
   http.listen(process.env.PORT || 5000, function () {
     console.log('listening on: ' + process.env.PORT || 5000);
   });
